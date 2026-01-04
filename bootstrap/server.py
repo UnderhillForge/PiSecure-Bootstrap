549817e7d98e8ae3d778107894f3c142f88a7484
J Stekervetz <jws@Js-Mac-mini.local>
2026-01-04 11:17:39 -0500

    Fix Railway deployment - use PORT env var and disable debug in production

--- bootstrap/server.py
+++ bootstrap/server.py
@@ -100,4 +100,7 @@ def hello():
     '''
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    import os
+    port = int(os.environ.get('PORT', 8080))
+    debug = os.environ.get('FLASK_ENV') == 'development'
+    app.run(host='0.0.0.0', port=port, debug=debug)
